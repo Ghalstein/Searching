b1cc3097a6611bb7691ad4db37d5d04e2c433b49
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-26 19:14:28 -0400

    started working on the public search function for EEI

--- ElementEqualsIndex.java
+++ ElementEqualsIndex.java
@@ -1,6 +1,19 @@
+/**************************************
+Assumes that the array is already sorted.
+Binary search traversal through the array 
+to find the element that mataches index.
+****************************************/
+
+import java.util.*;
+
 public class ElementEqualsIndex {
 
+
+	public static int search(ArrayList<Integer> arr) {
+
+	}
+
 	public static void main(String[] args) {
-		
+
 	}
 }
