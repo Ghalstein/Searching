87cc117db0eef451f813861f406b12ce254ecca4
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-27 07:08:46 -0400

    added some conditions for the private int sqrt function

--- IntegerSquareRoot.java
+++ IntegerSquareRoot.java
@@ -3,7 +3,15 @@ public class IntegerSquareRoot {
 	// private function for calling sqrt recursively
 	private static int sqrt(int n, int l, int m, int h) {
 
-		
+		// handles any out of bounds errors
+		if (n < 0 || l < 0 || l > h) {
+			return -1;
+		}
+
+		// if the sqaure root is found;
+		if (n / m == m) {
+			return m;
+		}
 	}
 
 	// finds the int square root
