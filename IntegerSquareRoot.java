146829ae785d563b5778ac5444c4805775b6080c
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-27 07:04:51 -0400

    made the public call to the private sqrt function

--- IntegerSquareRoot.java
+++ IntegerSquareRoot.java
@@ -1,6 +1,19 @@
 public class IntegerSquareRoot {
 
-	
+	// private function for calling sqrt recursively
+	private static int sqrt(int n, int l, int m, int h) {
+
+		
+	}
+
+	// finds the int square root
+	public static int sqrt(int num) {
+		int low = 0;
+		int high = int;
+		int med = low + (high - low) / 2;
+		return sqrt(num, low, med, high);
+
+	}
 
 	public static void main(String[] args) {
 
