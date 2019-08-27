6cf588ca3a37e907c62fc717e406cfcf3a127d33
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-26 22:18:05 -0400

    unit tested and working

--- ElementEqualsIndex.java
+++ ElementEqualsIndex.java
@@ -40,6 +40,14 @@ public class ElementEqualsIndex {
 	}
 
 	public static void main(String[] args) {
-
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(0);
+		arr.add(2);
+		arr.add(3);
+		arr.add(4);
+		arr.add(5);
+		arr.add(6);
+		arr.add(7);
+		System.out.println(search(arr));
 	}
 }
