8c0fe1c228638cf114d5d31a76bf6fcf53114b9d
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-27 22:19:38 -0400

    made the public sqrt function for RSQRT

--- RealSquareRoot.java
+++ RealSquareRoot.java
@@ -1,6 +1,25 @@
 public class RealSquareRoot {
 
+	public static double sqrt(double num) {
+		// declaring the low and high before giving them real value
+		double low = 0;
+		double high = 0;
+
+		if (num < 1.0) {
+			low = num;
+			high = 1.0;
+		}
+		else {
+			low = 1.0;
+			high = num;
+		}
+
+		double mid = low + (high - low) / 2.0;
+
+		sqrt(num, low, mid, high);
+	}
+
 	public static void main(String[] args) {
-		
+		System.out.println(sqrt(69));
 	}
 }
